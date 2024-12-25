aae6d8acda348c774530fa974ccd0e925bbed736
Dan <decisivedaniel@proton.me>
2024-12-25 02:32:27 +0100

    Part 1 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,18 +4,35 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        panic!("Not enough arguments!");
+    }
     let file_path = &args[1];
 
     let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file")
-    .split_whitespace();
+    .expect("Should have been able to read the file");
 
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
 
-    
+    for row in contents.split('\n') {
+        let (lstr, rstr) = row.split_once(' ')
+        .expect("Expecting a space in the row");
+        
+        println!("left: {lstr}, right: {rstr}");
+        left.push(lstr.trim().parse::<i32>().unwrap());
+        right.push(rstr.trim().parse::<i32>().unwrap());
+    }
 
     left.sort();
     right.sort();
-    println!("Hello, world!");
+
+    let mut sum = 0;
+    let mut count = 0;
+    while count < left.len() {
+        sum += (left[count] - right[count]).abs();
+        count += 1;
+    }
+    
+    println!("Sum of diff is: {sum}");
 }
